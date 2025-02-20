c5d4179481d0c6e5c2e6bcdec0add0c3a3d492d6
Jessica Li <saltsweetjk@JessicadeMacBook-Pro.local>
2025-02-20 16:40:40 +0000

    create summary table

--- bootcamp-demonstration analysis.R
+++ bootcamp-demonstration analysis.R
@@ -2,3 +2,15 @@ library(dplyr)
 library(readr)
 
 lul2_d <- read_csv('../data/life-under-lockdown-wave2-extract2.csv')
+
+lul2_d %>%
+  mutate(know_facebook = ordered(
+    know_facebook,
+    levels=c(
+      'Nothing at all',
+      'Not very much',
+      'A fair amount',
+      'A great deal'
+    ))) %>%
+  group_by(know_facebook) %>%
+  summarise(n=n(),Beliefs=mean(beliefs))
